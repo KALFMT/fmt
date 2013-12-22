56121daeb86cb02d88ae3169e39da17309907e29
Turingian <berenoguz@superonline.com>
2013-12-22 22:59:20 +0200

    Update mkclass.php

--- scripts/mkclass.php
+++ scripts/mkclass.php
@@ -26,39 +26,144 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-$type = "RealNumber";
-$attr = array("force","acceleration","mass","volume","density","weight",
+/*
+ * PART 1- CONFIGURATION
+ * Change related variables to create C++ code for Larus Project
+ * $name -> Name of the target class
+ * $attributes -> An array that includes attributes. Indexes will be named after
+ * 					attributes' types.
+ */
+$name = "AbstractSolidObject";
+$brief = "Solid Object class that has physcial traits";
+$attributes = array("RealNumber" => array(
+	 "force","acceleration","mass","volume","density","weight",
     "angular_momentum","momentum","surface_area","pressure","elascity",
     "electrical_charge","electrical_field","electrical_potential",
     "electrical_resistivity","electrical_impedance","electrical_resistance",
     "electrical_current","fluidity","hardness","temperature","velocity","speed",
-    "tension","plane_angle","melting_point","specific_heat","heat_capacity","plasticity","speed_of_sound");
-$name = "AbstractSolidObject";
-$attrs="";
+	 "tension","plane_angle","melting_point","specific_heat","heat_capacity",
+	 "plasticity","speed_of_sound"));
+	 
+/*
+ * PART 2- IMPLEMENTATION
+ * This part will prepare necessary parts of the code that will be created.
+ */
+
+$privates="";
 $getters="";
 $setters="";
+$setter_implemens="";
+$getter_implemens="";
 
-foreach($attr as $att)
+foreach($attributes as $type=>$atts)
 {
-	$setters .= "\t\tvoid set_$att(const $type& par);\n";
-	$getters .= "\t\t$type get_$att() const;\n";
-	$attrs .= "\t\t$type $att;\n";
+	foreach($atts as $att)
+	{
+   	$setters .= "\t\t\tvoid set_$att(const $type& par);\n";
+      $getters .= "\t\t\t$type get_$att() const;\n";
+      $privates .= "\t\t\t$type $att;\n";
+      $setter_implemens .= "\tvoid $name::set_$att(const $type& par)\n\t{\n/*\n\t}\n\n";
+      $getter_implemens .= "\t$type get_$att() const\n\t{\n\n\t}\n\n";
+   }
 }
 
+/*
+ * PART 3- CODE DUMP
+ * This part echoes both declaration and implementation codes of the class in C++
+ */
+
 echo <<<EOF
-class $name
+/**
+ * @file   $name.hpp
+ * @author Beren Oguz, Alptug Ulugol (kadikoyanadoluieee@gmail.com)
+ * @date   December, 2013
+ * @brief  This file includes the declaration of AbstractSolidObject Class.
+ * @copyright
+Copyright (c) 2013, 2014 Beren Oguz and Alptug Ulugol
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of KAL IEEE nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL BEREN OGUZ OR ALPTUG ULUGOL BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @class $name
+ * @brief $brief
+ */
+
+namespace larus
 {
-	public:
-		$name();
-		$name(const $name&);
-		~$name();
-		
+	class $name
+	{
+       	 public:
+\t\t\t$name();
+\t\t\t$name(const $name&);
+\t\t\t~$name();
+                
 $getters
 $setters
-	
-	private:
-$attrs
-};
+        
+        	private:
+$privates
+	};
+}
+
+/**
+ * @file   $name.cpp
+ * @author Beren Oguz, Alptug Ulugol (kadikoyanadoluieee@gmail.com)
+ * @date   December, 2013
+ * @brief  This file includes the implementation of AbstractSolidObject Class.
+ * @copyright
+Copyright (c) 2013, 2014 Beren Oguz and Alptug Ulugol
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of KAL IEEE nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL BEREN OGUZ OR ALPTUG ULUGOL BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace larus
+{
+$setter_implemens
+$getter_implemens
+}
 EOF;
 ?>
 </pre>
