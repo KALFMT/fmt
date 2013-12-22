d458df6bad4b8e19ec17070c763a2b2f25c40444
Turingian <berenoguz@superonline.com>
2013-12-22 23:05:07 +0200

    Update mkclass.php

--- scripts/mkclass.php
+++ scripts/mkclass.php
@@ -63,7 +63,7 @@ foreach($attributes as $type=>$atts)
       $getters .= "\t\t\t$type get_$att() const;\n";
       $privates .= "\t\t\t$type $att;\n";
       $setter_implemens .= "\tvoid $name::set_$att(const $type& par)\n\t{\n\t\t/*\n\t\t * @TODO\n\t\t * Implement this method!\n\t\t */\n\t}\n\n";
-      $getter_implemens .= "\t$type get_$att() const\n\t{\n\n\t}\n\n";
+      $getter_implemens .= "\t$type get_$att() const\n\t{\n\t\t/*\n\t\t * @TODO\n\t\t * Implement this method!\n\t\t */\n\t}\n\n";
    }
 }
 
