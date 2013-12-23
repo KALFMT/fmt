2c3eaf8ad9c4087ddc5d294d49bfc74fc9fe0e2b
apchubey <alptugulugol@gmail.com>
2013-12-23 23:18:34 +0200

    Update mkclass.py
    New Feature: Script directly creates the header file and the source file.

--- scripts/mkclass.py
+++ scripts/mkclass.py
@@ -66,12 +66,22 @@ for i in range(0,attlength):
     setter_implemens.append("\tvoid "+name+"::set_"+attributes[i]+"(const "+typeofvar+"& par)\n\t{\n\t\t/*\n\t\t * @TODO\n\t\t * Implement this method!\n\t\t */\n\t}\n\n")
     getter_implemens.append("\t"+typeofvar+" get_"+attributes[i]+"() const\n\t{\n\t\t/*\n\t\t * @TODO\n\t\t * Implement this method!\n\t\t */\n\t}\n\n")
 
-def printarray(array0):
 
-    "this function prints arrays"
+def writehpp(array0):
+
+    """this function writes into a .hpp file"""
 
     for elm in array0:
-        print(elm)
+        hpp_file.write(elm)
+    return
+
+
+def writecpp(array0):
+
+    """this function writes into a .cpp file"""
+
+    for elm in array0:
+        cpp_file.write(elm)
     return
 
 """
@@ -79,102 +89,104 @@ def printarray(array0):
 * This part echoes both declaration and implementation codes of the class in C++
 """
 
-print("""/**
-* @file """+name+""".hpp
-* @author Beren Oguz, Alptug Ulugol (kadikoyanadoluieee@gmail.com)
-* @date December, 2013
-* @brief This file includes the declaration of """+name+""" Class.
-* @copyright
-Copyright (c) 2013, 2014 Beren Oguz and Alptug Ulugol
-All rights reserved.
+with open(name+".hpp", "wt") as hpp_file:
+    hpp_file.write("""/**
+    * @file """+name+""".hpp
+    * @author Beren Oguz, Alptug Ulugol (kadikoyanadoluieee@gmail.com)
+    * @date December, 2013
+    * @brief This file includes the declaration of """+name+""" Class.
+    * @copyright
+    Copyright (c) 2013, 2014 Beren Oguz and Alptug Ulugol
+    All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-  * Redistributions of source code must retain the above copyright
-    notice, this list of conditions and the following disclaimer.
-  * Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the
-    documentation and/or other materials provided with the distribution.
-  * Neither the name of KAL IEEE nor the
-    names of its contributors may be used to endorse or promote products
-    derived from this software without specific prior written permission.
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+      * Neither the name of KAL IEEE nor the
+        names of its contributors may be used to endorse or promote products
+        derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL BEREN OGUZ OR ALPTUG ULUGOL BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL BEREN OGUZ OR ALPTUG ULUGOL BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    */
 
-/**
-* @class """+name+"""
-* @brief """+brief+"""
-*/
+    /**
+    * @class """+name+"""
+    * @brief """+brief+"""
+    */
 
-#include "defines.h"
+    #include "defines.h"
 
-namespace larus
-{
-        class $name
-        {
-         public:
-\t\t\t"""+name+"""();
-\t\t\t"""+name+"""(const """+name+"""&);
-\t\t\t~"""+name+"""();
-""")
-printarray(getters)
-printarray(setters)
-print("""
-        private:
-""")
-printarray(privates)
-print("""
-        };
-}
+    namespace larus
+    {
+            class $name
+            {
+             public:
+    \t\t\t"""+name+"""();
+    \t\t\t"""+name+"""(const """+name+"""&);
+    \t\t\t~"""+name+"""();
+    """)
+    writehpp(getters)
+    writehpp(setters)
+    hpp_file.write("""
+            private:
+    """)
+    writehpp(privates)
+    hpp_file.write("""
+            };
+    }
 
-""")
+    """)
 
-print("""/**
-* @file """+name+""".cpp
-* @author Beren Oguz, Alptug Ulugol (kadikoyanadoluieee@gmail.com)
-* @date December, 2013
-* @brief This file includes the implementation of """+name+""" Class.
-* @copyright
-Copyright (c) 2013, 2014 Beren Oguz and Alptug Ulugol
-All rights reserved.
+with open(name+".cpp", "wt") as cpp_file:
+    cpp_file.write("""/**
+    * @file """+name+""".hpp
+    * @author Beren Oguz, Alptug Ulugol (kadikoyanadoluieee@gmail.com)
+    * @date December, 2013
+    * @brief This file includes the declaration of """+name+""" Class.
+    * @copyright
+    Copyright (c) 2013, 2014 Beren Oguz and Alptug Ulugol
+    All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-* Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
-* Neither the name of KAL IEEE nor the
-names of its contributors may be used to endorse or promote products
-derived from this software without specific prior written permission.
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+      * Neither the name of KAL IEEE nor the
+        names of its contributors may be used to endorse or promote products
+        derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL BEREN OGUZ OR ALPTUG ULUGOL BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL BEREN OGUZ OR ALPTUG ULUGOL BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    */
 
-#include "larus/"""+name+""".hpp"
+    #include "larus/"""+name+""".hpp"
 
-namespace larus
-{""")
-printarray(setter_implemens)
-printarray(getter_implemens)
-print("}")
+    namespace larus
+    {""")
+    writecpp(setter_implemens)
+    writecpp(getter_implemens)
+    cpp_file.write("}")
