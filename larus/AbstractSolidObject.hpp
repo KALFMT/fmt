1573540d1b489d2b8de664a433aa0c1aa461c64b
Turingian <berenoguz@superonline.com>
2013-12-28 20:15:31 +0200

    Update AbstractSolidObject.hpp

--- larus/AbstractSolidObject.hpp
+++ larus/AbstractSolidObject.hpp
@@ -1,22 +1,22 @@
 /**
-* @file AbstractSolidObject.hpp
-* @author Beren Oguz, Alptug Ulugol (kadikoyanadoluieee@gmail.com)
-* @date December, 2013
-* @brief This file includes the declaration of AbstractSolidObject Class.
-* @copyright
+ * @file   AbstractSolidObject.hpp
+ * @author Beren Oguz, Alptug Ulugol (kadikoyanadoluieee@gmail.com)
+ * @date   December, 2013
+ * @brief  This file includes the declaration of AbstractSolidObject Class.
+ * @copyright
 Copyright (c) 2013, 2014 Beren Oguz and Alptug Ulugol
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-* Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
-* Neither the name of KAL IEEE nor the
-names of its contributors may be used to endorse or promote products
-derived from this software without specific prior written permission.
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of KAL IEEE nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -28,84 +28,55 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
 /**
-* @class AbstractSolidObject
-* @brief Solid Object class that has physcial traits
-*/
+ * @class AbstractSolidObject
+ * @brief Solid Object class that has physcial traits
+ */
 
 #include "defines.h"
 #include "RealNumber.hpp"
 
 namespace larus
 {
-        class AbstractSolidObject
-        {
-         public:
+	class AbstractSolidObject
+	{
+       	 public:
 			AbstractSolidObject();
 			AbstractSolidObject(const AbstractSolidObject&);
 			~AbstractSolidObject();
+                
 			const RealNumber& get_force() const;
-			RealNumber& get_force();
 			const RealNumber& get_acceleration() const;
-			RealNumber& get_acceleration();
 			const RealNumber& get_mass() const;
-			RealNumber& get_mass();
 			const RealNumber& get_volume() const;
-			RealNumber& get_volume();
 			const RealNumber& get_density() const;
-			RealNumber& get_density();
 			const RealNumber& get_weight() const;
-			RealNumber& get_weight();
 			const RealNumber& get_angular_momentum() const;
-			RealNumber& get_angular_momentum();
 			const RealNumber& get_momentum() const;
-			RealNumber& get_momentum();
 			const RealNumber& get_surface_area() const;
-			RealNumber& get_surface_area();
 			const RealNumber& get_pressure() const;
-			RealNumber& get_pressure();
 			const RealNumber& get_elascity() const;
-			RealNumber& get_elascity();
 			const RealNumber& get_electrical_charge() const;
-			RealNumber& get_electrical_charge();
 			const RealNumber& get_electrical_field() const;
-			RealNumber& get_electrical_field();
 			const RealNumber& get_electrical_potential() const;
-			RealNumber& get_electrical_potential();
 			const RealNumber& get_electrical_resistivity() const;
-			RealNumber& get_electrical_resistivity();
 			const RealNumber& get_electrical_impedance() const;
-			RealNumber& get_electrical_impedance();
 			const RealNumber& get_electrical_resistance() const;
-			RealNumber& get_electrical_resistance();
 			const RealNumber& get_electrical_current() const;
-			RealNumber& get_electrical_current();
 			const RealNumber& get_fluidity() const;
-			RealNumber& get_fluidity();
 			const RealNumber& get_hardness() const;
-			RealNumber& get_hardness();
 			const RealNumber& get_temperature() const;
-			RealNumber& get_temperature();
 			const RealNumber& get_velocity() const;
-			RealNumber& get_velocity();
 			const RealNumber& get_speed() const;
-			RealNumber& get_speed();
 			const RealNumber& get_tension() const;
-			RealNumber& get_tension();
 			const RealNumber& get_plane_angle() const;
-			RealNumber& get_plane_angle();
 			const RealNumber& get_melting_point() const;
-			RealNumber& get_melting_point();
 			const RealNumber& get_specific_heat() const;
-			RealNumber& get_specific_heat();
 			const RealNumber& get_heat_capacity() const;
-			RealNumber& get_heat_capacity();
 			const RealNumber& get_plasticity() const;
-			RealNumber& get_plasticity();
 			const RealNumber& get_speed_of_sound() const;
-			RealNumber& get_speed_of_sound();
 
 			void set_force(const RealNumber& par);
 			void set_acceleration(const RealNumber& par);
@@ -138,7 +109,8 @@ namespace larus
 			void set_plasticity(const RealNumber& par);
 			void set_speed_of_sound(const RealNumber& par);
 
-        private:
+        
+        	private:
 			RealNumber force;
 			RealNumber acceleration;
 			RealNumber mass;
@@ -170,5 +142,5 @@ namespace larus
 			RealNumber plasticity;
 			RealNumber speed_of_sound;
 
-        };
+	};
 }
