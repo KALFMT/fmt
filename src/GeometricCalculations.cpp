b0142958b6d06b6f7f9910ad93c0f28beac655e1
apchubey <alptugulugol@gmail.com>
2014-02-25 23:47:28 +0200

    Create GeometricCalculations.cpp

---
+++ src/GeometricCalculations.cpp
@@ -0,0 +1,46 @@
+/**
+ * @file   GeometricCalculations.hpp
+ * @author Beren Oguz, Alptug Ulugol (kadikoyanadoluieee@gmail.com)
+ * @date   February, 2014
+ * @brief  This file includes the implementation of Geometric Calculations.
+ * @copyright
+ Copyright (c) 2013, 2014 Beren Oguz and Alptug Ulugol
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of KAL IEEE nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL BEREN OGUZ OR ALPTUG ULUGOL BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "larus/GeometricCalculations.hpp"
+
+namespace larus
+{
+    RealNumber rad_to_deg(RealNumber rad)
+    {
+        return rad*180/M_PI;
+    }
+    
+    RealNumber deg_to_rad(RealNumber deg)
+    {
+        return deg*M_PI/180;
+    }
+}
