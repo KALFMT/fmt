ea3797983b823ca105e6015baea4217ac77faf24
apchubey <alptugulugol@gmail.com>
2014-03-01 22:30:08 +0200

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-
+#include "larus.hpp"
+#include <utility>
+#include <vector>
 bool vectors_test();
 bool kinematics_1_test();
+std::pair<larus::Boolean, std::vector<larus::String>> RealNumber_test();
 
 int main()
 {
-
+    
     bool vectors = vectors_test();
     bool kinematics_1 = kinematics_1_test();
-
+    
+    
     std::cout << std::endl;
-
+    
     std::cout << "Test 1 (Vectors): ";
     if(vectors)
     {
@@ -21,7 +25,7 @@ int main()
         std::cerr << "FAILED" << std::endl;
         return 1;
     }
-
+    
     std::cout << "Test 2 (Kinematics 1): ";
     if(kinematics_1)
     {
@@ -32,4 +36,27 @@ int main()
         std::cerr << "FAILED" << std::endl;
         return 1;
     }
+    
+    std::cout << "Test 3 (RealNumbers): ";
+    
+    std::pair<larus::Boolean, std::vector<larus::String>> test = RealNumber_test();
+    
+    if(test.first)
+    {
+        std::cout << "SUCCESS" << std::endl;
+    }
+    else
+    {
+        std::cout << "FAILED" << std::endl;
+        std::cout << "RealNumbers Test Result:" << std::endl;
+        
+        for (std::vector<larus::String>::const_iterator it = test.second.begin(); it != test.second.end(); it++)
+        {
+            std::cout<< *it << std::endl;
+        }
+        
+        return 1;
+    }
+    
+
 }
