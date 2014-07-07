96e345d7d8e24268e39dbc44def79310c889d7b5
Turingian <berenoguz@superonline.com>
2014-07-07 23:02:19 +0300

    Adds standard string library of C++ to test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,19 +2,21 @@
 #include "larus.hpp"
 #include <utility>
 #include <vector>
+#include <string>
+
 bool vectors_test();
 bool kinematics_1_test();
 std::pair<larus::Boolean, std::vector<larus::String>> RealNumber_test();
 
 int main()
 {
-    
+
     bool vectors = vectors_test();
     bool kinematics_1 = kinematics_1_test();
-    
-    
+
+
     std::cout << std::endl;
-    
+
     std::cout << "Test 1 (Vectors): ";
     if(vectors)
     {
@@ -25,7 +27,7 @@ int main()
         std::cerr << "FAILED" << std::endl;
         return 1;
     }
-    
+
     std::cout << "Test 2 (Kinematics 1): ";
     if(kinematics_1)
     {
@@ -36,11 +38,11 @@ int main()
         std::cerr << "FAILED" << std::endl;
         return 1;
     }
-    
+
     std::cout << "Test 3 (RealNumbers): ";
-    
+
     std::pair<larus::Boolean, std::vector<larus::String>> test = RealNumber_test();
-    
+
     if(test.first)
     {
         std::cout << "SUCCESS" << std::endl;
@@ -49,14 +51,14 @@ int main()
     {
         std::cout << "FAILED" << std::endl;
         std::cout << "RealNumbers Test Result:" << std::endl;
-        
+
         for (std::vector<larus::String>::const_iterator it = test.second.begin(); it != test.second.end(); it++)
         {
             std::cout<< *it << std::endl;
         }
-        
+
         return 1;
     }
-    
+
 
 }
