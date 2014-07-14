be5e9fbae9a13f99ff2588094546abcb9d731a79
Beren Oğuz <berenoguz@superonline.com>
2014-07-14 23:52:01 +0300

    Variable class constructor and destructor implemented

---
+++ src/Variable.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file   Variable.cpp
+ * @author Beren Oguz, Alptug Ulugol (kadikoyanadoluieee@gmail.com)
+ * @date   December, 2013
+ * @brief  This file includes the implementation of Variable Class.
+ * @copyright Copyright (c) 2013, 2014 Alptuğ Ulugöl, Beren Oğuz
+ * @license Lesser GNU Public License (LGPL) <https://www.gnu.org/licenses/lgpl.html> (see LICENSE file)
+ */
+
+namespace larus
+{
+    Variable(larus::String name)
+    :symbol(name), value(0)
+    {
+
+    }
+
+    ~Variable()
+    {
+
+    }
+}
